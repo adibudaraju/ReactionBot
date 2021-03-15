5c5222a171744faac79e3c94c1734e51e39525ae
Adi Budaraju <adibudaraju@gmail.com>
2021-03-15 14:53:26 -0700

    Commit to hide token
    Signed-off-by: Adi Budaraju <adibudaraju@gmail.com>

--- src/main/java/com/adibuda/bot/Main.java
+++ src/main/java/com/adibuda/bot/Main.java
@@ -4,10 +4,14 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) throws LoginException {
-        JDA jda = JDABuilder.createDefault("ODIxMTA1NDAwMTg2MjczODE0.YE-3_g.fkTXnWthnFuaAbnnmhMMDpoI4W0").build();
+    public static void main(String[] args) throws LoginException, FileNotFoundException {
+        String token = new Scanner(new File( "src/main/java/com/adibuda/bot/token.txt")).nextLine();
+        JDA jda = JDABuilder.createDefault(token).build();
         jda.addEventListener(new BozoEvent());
     }
 }
